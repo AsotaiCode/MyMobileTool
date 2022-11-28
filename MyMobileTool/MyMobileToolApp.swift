9bc05ac13ee8b701329f96c9390ef1ff04f800ef
AsotaiCode <taisei.aso.work@gmail.com>
2022-11-29 08:52:40 +0900

    Initial Commit

---
+++ MyMobileTool/MyMobileToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMobileToolApp.swift
+//  MyMobileTool
+//
+//  Created by 麻生 大聖 on 2022/11/29.
+//
+
+import SwiftUI
+
+@main
+struct MyMobileToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
